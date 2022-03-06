ed12f1238cb27e793f58c0464003de5620d207ae
Tinka8 <lenmatka818@gmail.com>
2022-03-06 19:40:29 +0100

    map

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  content: ["./index.html", "./index-menu.html", "./index-menu-sub1.html", "index-home.html"],
+  content: ["./index.html", "./index-menu.html", "./index-menu-sub1.html", "index-home.html", "./index-home2.html"],
   theme: {
     extend: {
       colors: {
